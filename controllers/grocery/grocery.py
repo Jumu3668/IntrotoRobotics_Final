1b1a5e19480357dcb014a4ecb98c3fd4ed56e238
Justin Murillo <boxmonkey13@outlook.com>
2022-04-25 11:58:59 -0600

    Added interchangeable gps and odometry

--- controllers/grocery/grocery.py
+++ controllers/grocery/grocery.py
@@ -1,28 +1,28 @@
-"""grocery controller."""
-
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
-from controller import Robot, Keyboard, Lidar, RangeFinder, Motor, Camera
+"""Final Lab Controller"""
+from controller import Robot, Motor, Camera, RangeFinder, Lidar, Keyboard, Display
 import math
+import sys
 import numpy as np
-
-
-#Initialization
-print("=== Initializing Grocery Shopper...")
-#Consts
+from matplotlib import pyplot as plt
+import matplotlib.patches as patches
+from scipy.signal import convolve2d # Uncomment if you want to use something else for finding the configuration space
 MAX_SPEED = 7.0  # [rad/s]
 MAX_SPEED_MS = 0.633 # [m/s]
 AXLE_LENGTH = 0.4044 # m
 MOTOR_LEFT = 10
 MOTOR_RIGHT = 11
 N_PARTS = 12
-LIDAR_ANGLE_BINS = 667
-LIDAR_SENSOR_MAX_RANGE = 5.5 # Meters
-LIDAR_ANGLE_RANGE = math.radians(240)
+
+LIDAR_ANGLE_BINS = 667 #number of rays
+LIDAR_SENSOR_MAX_RANGE = 2.75 # Meters
+LIDAR_ANGLE_RANGE = math.radians(240) #given to us how?
+
+
+##### vvv [Begin] Do Not Modify vvv #####
 
 # create the Robot instance.
 robot = Robot()
-
+# display = Display("New_Display")
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
@@ -41,28 +41,31 @@ for i in range(N_PARTS):
     robot_parts[i].setPosition(float(target_pos[i]))
     robot_parts[i].setVelocity(robot_parts[i].getMaxVelocity() / 2.0)
 
-# Enable Camera
-camera = robot.getDevice('camera')
-camera.enable(timestep)
-camera.recognitionEnable(timestep)
+# The Tiago robot has a couple more sensors than the e-Puck
+# Some of them are mentioned below. We will use its LiDAR for Lab 5
 
-# Enable GPS and compass localization
+# range = robot.getDevice('range-finder')
+# range.enable(timestep)
+# camera = robot.getDevice('camera')
+# camera.enable(timestep)
+# camera.recognitionEnable(timestep)
+lidar = robot.getDevice('Hokuyo URG-04LX-UG01')
+lidar.enable(timestep)
+lidar.enablePointCloud()
+
+# We are using a GPS and compass to disentangle mapping and localization
 gps = robot.getDevice("gps")
 gps.enable(timestep)
 compass = robot.getDevice("compass")
 compass.enable(timestep)
 
-# Enable LiDAR
-lidar = robot.getDevice('Hokuyo URG-04LX-UG01')
-lidar.enable(timestep)
-lidar.enablePointCloud()
-
-# Enable display
-display = robot.getDevice("display")
-
-#set up keybaord call
+# We are using a keyboard to remote control the robot
 keyboard = robot.getKeyboard()
 keyboard.enable(timestep)
+
+# The display is used to display the map. We are using 480x900 pixels to
+# map the 12x12m2 apartment
+display = robot.getDevice("display")
 
 # Odometry
 pose_x     = 0
@@ -73,43 +76,252 @@ vL = 0
 vR = 0
 
 lidar_sensor_readings = [] # List to hold sensor readings
-lidar_offsets = np.linspace(-LIDAR_ANGLE_RANGE/2., +LIDAR_ANGLE_RANGE/2., LIDAR_ANGLE_BINS)
+lidar_offsets = np.linspace(-LIDAR_ANGLE_RANGE/2., +LIDAR_ANGLE_RANGE/2., LIDAR_ANGLE_BINS) #position in radians of all lidar bins on robot
+# print(lidar_offsets)
 lidar_offsets = lidar_offsets[83:len(lidar_offsets)-83] # Only keep lidar readings not blocked by robot chassis
 
 map = None
+##### ^^^ [End] Do Not Modify ^^^ #####
+
+##################### IMPORTANT #####################
+# Set the mode here. Please change to 'autonomous' before submission
+# mode = 'manual' # Part 1.1: manual mode
+mode = 'manual'
+# mode = 'autonomous'
 
 
 
-# ------------------------------------------------------------------
-# Helper Functions
 
+###################
+#
+# Planner
+#
+###################
+if mode == 'planner':
+    map = np.load('map.npy')
+    pixels = 0
+    #map = np.fliplr(map)
+    map = np.rot90(map, 3)
+    plt.imshow(map)
+    print(map)
+    plt.show()
+    kernel_size = 12
+    Kernel = np.ones((kernel_size, kernel_size))
+    Convolved_map = convolve2d(map, Kernel, mode='same')
+    Convolved_map = (Convolved_map > 0.3).astype(int)
+    # Convolved_map = np.fliplr(Convolved_map)
+    # Convolved_map = np.rot90(Convolved_map)
+    # boxy_map = map.tolist()
+    # for x in range(len(boxy_map)):
+        # for y in range(len(boxy_map)):
+            # if boxy_map[x][y] == 1:
+                # pixels+= 1
+                #12-16 pixels
+                                                        
+                # draw box around 
+    print(pixels)
+    np.set_printoptions(threshold=sys.maxsize)
+    # print(Convolved_map.reshape(360,360).tolist())
+    plt.imshow(Convolved_map)
+    plt.show()
+    # print("test0")
+            
+    # Part 2.3: Provide start and end in world coordinate frame and convert it to map's frame
+    start_w = (4.46793, 8.05674) # (Pose_X, Pose_Z) in meters
+    end_w = (10.342,1.34638) # (Pose_X, Pose_Z) in meters
 
-modeManual = True #uncomment to put in manual mode
-#modeAuto = True #uncomment to put in manual mode
-# Main Loop
-while robot.step(timestep) != -1:
+    # Convert the start_w and end_w from the webots coordinate frame into the map frame
+    ratio = 12/360
+    start = (int(round(start_w[0] / ratio)), int(round(start_w[1] / ratio))) # (x, y) in 360x360 map
+    end = (int(round(end_w[0] / ratio)), int(round(end_w[1] / ratio))) # (x, y) in 360x360 map
+    # start = (167,185)
+    print(str(start))
+    print(str(end))
+    # end = (293, 316)
+    # print("test")
+    # Part 2.3: Implement A* or Dijkstra's Algorithm to find a path
     
-    if modeManual == True:
+    def path_planner(map, start, end):
+        #a* search from https://towardsdatascience.com/a-star-a-search-algorithm-eb495fb156bb
+        
+
+
+        pass
+
+    # Part 2.1: Load map (map.npy) from disk and visualize it
+
+
+    # Part 2.2: Compute an approximation of the “configuration space”
+
+
+    # Part 2.3 continuation: Call path_planner
+    map = map.tolist()
+    stuff = path_planner(map, start, end)
+    print(stuff)
+    # Part 2.4: Turn paths into waypoints and save on disk as path.npy and visualize it
+    waypoints = []
+
+######################
+#
+# Map Initialization
+#
+######################
+
+# Part 1.2: Map Initialization
+
+# Initialize your map data structure here as a 2D floating point array
+map = np.zeros((480,900)) # Replace None by a numpy 2D floating point array
+waypoints = []
+if mode == 'autonomous':
+    # Part 3.1: Load path from disk and visualize it
+    waypoints = [] # Replace with code to load your path
+
+state = 0 # use this to iterate through your path
+
+
+# localization_mode = 'gps'
+localization_mode = 'odometry'
+
+while robot.step(timestep) != -1 and mode != 'planner':
+
+    ###################
+    #
+    # Mapping
+    #
+    ###################
+
+    ################ v [Begin] Do not modify v ##################
+    # GPS BASED Ground truth pose
+    
+    if localization_mode == 'gps':
+        pose_x = gps.getValues()[2]
+        pose_y = gps.getValues()[0] 
+        display.setColor(int(0xFF0000))
+        display.drawPixel(int(100+pose_x*30),int(320-pose_y*30))
+        
+    n = compass.getValues()
+    pose_theta = -((math.atan2(n[0], n[2]))-1.5708)
+
+    lidar_sensor_readings = lidar.getRangeImage()
+    lidar_sensor_readings = lidar_sensor_readings[83:len(lidar_sensor_readings)-83]
+    # print(str(lidar_sensor_readings))
+    # print(len(lidar_sensor_readings))
+    for i, rho in enumerate(lidar_sensor_readings):
+        alpha = lidar_offsets[i] #get current lidar bin position in radians
+
+        # rho is single lidar sensor reading at position 'i'
+        if rho > LIDAR_SENSOR_MAX_RANGE:
+            continue #if rho value too far... skip to next number
+
+        # Convert detection from robot coordinates into world coordinates
+        wx = -math.cos(pose_theta - alpha) * rho + pose_x
+        wy = -math.sin(pose_theta - alpha) * rho + pose_y
+        # print("wx: " + str(wx))
+        # print("wy: " + str(wy))
+        # print(rho)
+        ################ ^ [End] Do not modify ^ ##################
+
+        #print("Rho: %f Alpha: %f rx: %f ry: %f wx: %f wy: %f" % (rho,alpha,rx,ry,wx,wy))
+
+        if rho < LIDAR_SENSOR_MAX_RANGE:
+            # Part 1.3: visualize map gray values.
+
+            # You will eventually REPLACE the following 2 lines with a more robust version of the map
+            # with a grayscale drawing containing more levels than just 0 and 1.
+            # display.setColor(0xFFFFFF)
+            
+            #convert world coordinates into display coordinates
+            dy = 320-int(wy*30)
+            dx = 100+int(wx*30)
+                          
+            # if dy > 900:
+                # dy = 900
+            # if dx > 480:
+                # dx = 480
+            # print("dy: " + str(dy))
+            # print("dx: " + str(dx))
+            # grayscale code    
+            val = map[dx-1][dy-1]
+            if val >= 1:
+                val = 1
+            else:
+                val += 0.0045
+                map[dx-1][dy-1] = val
+            
+            g = int(val* 255) # converting [0,1] to grayscale intensity [0,255]
+            color = g*256**2+g*256+g
+            display.setColor(color)
+            display.drawPixel(dx,dy) #draws from the top left corner(0,900)
+            
+    # Draw the robot's current pose on the 360x360 display
+    
+    
+
+    ###################
+    #
+    # Controller
+    #
+    ###################
+    if mode == 'manual':
         key = keyboard.getKey()
-        while(keyboard.getKey() != -1): 
-            if key == keyboard.LEFT:
-                print("Pressed Left")
-                vL = -MAX_SPEED
-                vR = MAX_SPEED
-            elif key == keyboard.RIGHT:
-                print("Pressed Right")
-                vL = MAX_SPEED
-                vR = -MAX_SPEED
-            elif key == keyboard.UP:
-                print("Pressed Forward")
-                vL = MAX_SPEED
-                vR = MAX_SPEED
-            elif key == keyboard.DOWN:
-                print("Pressed Back")
-                vL = -MAX_SPEED
-                vR = -MAX_SPEED
-            else: # slow down
-                vL *= 0.75
-                vR *= 0.75
-    robot.getDevice("wheel_left_joint").setVelocity(vL)
-    robot.getDevice("wheel_right_joint").setVelocity(vR)
+        while(keyboard.getKey() != -1): pass
+        if key == keyboard.LEFT :
+            vL = -MAX_SPEED
+            vR = MAX_SPEED
+        elif key == keyboard.RIGHT:
+            vL = MAX_SPEED
+            vR = -MAX_SPEED
+        elif key == keyboard.UP:
+            vL = MAX_SPEED
+            vR = MAX_SPEED
+        elif key == keyboard.DOWN:
+            vL = -MAX_SPEED
+            vR = -MAX_SPEED
+        elif key == ord(' '):
+            vL = 0
+            vR = 0
+        elif key == ord('S'):
+            # Part 1.4: Filter map and save to filesystem
+            savemap = (map > 0.3).astype(int)
+            np.save('map', savemap)
+            print("Map file saved")
+        elif key == ord('L'):
+            # You will not use this portion in Part 1 but here's an example for loading saved a numpy array
+            map = np.load("map.npy")
+            print("Map loaded")
+        else: # slow down
+            vL *= 0.75
+            vR *= 0.75
+    else: # not manual mode
+        # Part 3.2: Feedback controller
+        #STEP 1: Calculate the error
+        rho = 0
+        alpha = -(math.atan2(waypoint[state][1]-pose_y,waypoint[state][0]-pose_x) + pose_theta)
+
+
+        #STEP 2: Controller
+        dX = 0
+        dTheta = 0
+
+        #STEP 3: Compute wheelspeeds
+        vL = 0
+        vR = 0
+
+        # Normalize wheelspeed
+        # (Keep the wheel speeds a bit less than the actual platform MAX_SPEED to minimize jerk)
+
+
+    # Odometry code. Don't change vL or vR speeds after this line.
+    # We are using GPS and compass for this lab to get a better pose but this is how you'll do the odometry
+    if localization_mode == 'odometry':
+        pose_x -= (vL+vR)/2/MAX_SPEED*MAX_SPEED_MS*timestep/1000.0*math.cos(pose_theta)
+        pose_y -= (vL+vR)/2/MAX_SPEED*MAX_SPEED_MS*timestep/1000.0*math.sin(pose_theta)
+        pose_theta += (vR-vL)/AXLE_LENGTH/MAX_SPEED*MAX_SPEED_MS*timestep/1000.0
+        display.setColor(int(0xFFF000))
+        display.drawPixel(int(240-pose_x*30),int(700-pose_y*30))
+
+    # print("X: %f Z: %f Theta: %f" % (pose_x, pose_y, pose_theta))
+
+    # Actuator commands
+    robot_parts[MOTOR_LEFT].setVelocity(vL)
+    robot_parts[MOTOR_RIGHT].setVelocity(vR)
